78da61578b0b70cdf669d7010030945c53ad8f91
ln64 <87522220+ln64@users.noreply.github.com>
2026-03-15 16:51:00 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '苍书文档',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: '使用指南',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: '示例指南', slug: 'guides/example' },
-					],
-				},
-				{
-					label: '参考文档',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: '我的资源站',
+      // 设置简体中文为默认语言
+      defaultLocale: 'root',
+      locales: {
+        // 将中文设为 root 语言，这样网址就是 /xxx 而不是 /zh-cn/xxx
+        root: {
+          label: '简体中文',
+          lang: 'zh-CN',
+        },
+      },
+    }),
+  ],
 });
